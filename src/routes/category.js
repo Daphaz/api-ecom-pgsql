0d98afec6098a03e12cfd4fce19f26b1839626dd
Daphaz <bonnet.damien@ymail.com>
2021-04-24 00:39:16 +0200

    feat(category): add crud route

--- src/routes/category.js
+++ src/routes/category.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
+import { verifyToken, isAdmin } from "../utils";
 const category = require("../controllers/CategoryController");
 const router = Router();
 
 router.get("/all", category.getCategories);
-router.post("/create", category.createCategory);
-router.put("/update/:id", category.updateCategory);
-router.delete("/delete", category.deleteCategory);
+router.post("/create", verifyToken, isAdmin, category.createCategory);
+router.put("/update/:id", verifyToken, isAdmin, category.updateCategory);
+router.delete("/delete", verifyToken, isAdmin, category.deleteCategory);
 
 export default router;

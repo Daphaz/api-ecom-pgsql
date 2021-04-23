5b0f30d04a09fd452b42fcfd1ccdfcd4d6acd13f
Daphaz <bonnet.damien@ymail.com>
2021-04-23 17:00:07 +0200

    feat(resetPassword): create route for create and verify token

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ import user from "./user";
 import auth from "./auth";
 import category from "./category";
 import product from "./product";
+import resetPassword from "./resetPassword";
 
 export default {
 	user,
 	auth,
 	category,
 	product,
+	resetPassword,
 };

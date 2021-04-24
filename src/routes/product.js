de844b0946710791f1bf57f7ab87fd3756bd819b
Daphaz <bonnet.damien@ymail.com>
2021-04-24 22:24:51 +0200

    feat(product): add midlleware for authentification and image storage

--- src/routes/product.js
+++ src/routes/product.js
@@ -1,12 +1,24 @@
 import { Router } from "express";
+import { verifyToken, isAdmin, uploadImage } from "../utils";
 const product = require("../controllers/ProductController");
 const router = Router();
 
 router.get("/all", product.getProducts);
-router.post("/create", product.createProduct);
-router.put("/set-category/:id", product.setCategoryProduct);
-router.put("/is-best/:id", product.setIsBest);
-router.put("/update/:id", product.updateProduct);
-router.delete("/delete", product.deleteProduct);
+router.post(
+	"/create",
+	verifyToken,
+	isAdmin,
+	uploadImage.single("illustration"),
+	product.createProduct
+);
+router.put(
+	"/set-category/:id",
+	verifyToken,
+	isAdmin,
+	product.setCategoryProduct
+);
+router.put("/is-best/:id", verifyToken, isAdmin, product.setIsBest);
+router.put("/update/:id", verifyToken, isAdmin, product.updateProduct);
+router.delete("/delete", verifyToken, isAdmin, product.deleteProduct);
 
 export default router;

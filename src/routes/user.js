2a29d1315ee2fb5749af40d5efa5c3335f2020b5
Daphaz <bonnet.damien@ymail.com>
2021-05-06 02:52:51 +0200

    feat(user): add route get all user

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 const user = require("../controllers/UserController");
 const router = Router();
 
+router.get("/all", user.getAllUser);
 router.get("/:id", user.getUser);
 router.post("/create", user.createUser);
 router.put("/update/:id", user.updateUser);

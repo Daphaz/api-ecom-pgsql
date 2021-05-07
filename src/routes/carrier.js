068d18b72295131c02d8b459e397791026b54993
Daphaz <bonnet.damien@ymail.com>
2021-05-08 00:32:27 +0200

    fix(carrier): add middleware

--- src/routes/carrier.js
+++ src/routes/carrier.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import { isAdmin } from "../utils";
+import { isAdmin, verifyToken } from "../utils";
 const carrier = require("../controllers/CarrierController");
 const router = Router();
 
-router.get("/all", carrier.getAllCarrier);
-router.get("/edit/:id", carrier.getCarrier);
-router.post("/create", isAdmin, carrier.createCarrier);
-router.put("/update/:id", isAdmin, carrier.updateCarrier);
-router.delete("/delete", isAdmin, carrier.deleteCarrier);
+router.get("/all", verifyToken, carrier.getAllCarrier);
+router.get("/edit/:id", verifyToken, carrier.getCarrier);
+router.post("/create", verifyToken, isAdmin, carrier.createCarrier);
+router.put("/update/:id", verifyToken, isAdmin, carrier.updateCarrier);
+router.delete("/delete", verifyToken, isAdmin, carrier.deleteCarrier);
 
 export default router;
